d6b254e1f131e718918f1ed7daa4127b918fe116
zqqiang <qiangzhaoqing@gmail.com>
2014-09-16 20:49:17 +0800

    delete assets/js

--- public/assets/js/dummy.js
+++
@@ -1,9 +0,0 @@
-(function(app) {
-	var dummyTemplate = [
-		'<div class="alert alert-danger" role="alert">Errors! Coming soon...</div>',
-	];
-
-	app.DummyView = Marionette.ItemView.extend({
-		template: dummyTemplate.join(''),
-	});
-})(Application);
