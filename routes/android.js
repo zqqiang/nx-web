37dbafdda967a17675d0e44b35eb95671cdeee4e
= <=>
2014-10-23 14:00:30 +0800

    add post

--- routes/android.js
+++ routes/android.js
@@ -2,29 +2,38 @@ var express = require('express');
 var mongoose = require('../db/db');
 var router = express.Router();
 
+var Cat = mongoose.model('Cat', {
+	name: String
+});
+
 router.use(function(req, res, next) {
-	console.log(req.originalUrl);
-	console.log(req.method);
+	console.log('originalUrl: ', req.originalUrl);
+	console.log('method: ', req.method);
 	next();
 });
 
-router.use('/', function(req, res) {
-	var Cat = mongoose.model('Cat', {
-		name: String
+router.get('/', function(req, res) {
+	Cat.find(function(err, cats) {
+		if (err) console.error(err);
+		res.json(cats);
 	});
+});
 
-	var kitty = new Cat({
-		name: 'Zildjian'
-	});
+router.post('/', function(req, res) {
+	console.log('body: ', req.body);
+	console.log('body.name: ', req.body.name);
 
+	var kitty = new Cat(req.body);
 	kitty.save(function(err) {
 		if (err) {
-			console.log(err);
+			console.error(err);
 		} else {
-			res.json({
-				message: 'insert kitty success!'
+			Cat.find({
+				name: req.body.name
+			}, function(err, cat) {
+				if (err) console.error(err);
+				res.json(cat);
 			});
-			console.log('suceess');
 		}
 	});
 });
