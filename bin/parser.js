fca0daad6eb6a7236983395ef74157fef188d671
= <=>
2014-10-28 17:34:18 +0800

    add dianping api

--- bin/parser.js
+++ bin/parser.js
@@ -2,11 +2,11 @@ var http = require('http');
 var cheerio = require('cheerio');
 
 var options = {
-	hostname: 'www.dianping.com',
-	path: '/search/category/2/30/g139',
-	headers: {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
-	}
+	hostname: 'api.dianping.com',
+	path: '/v1/deal/get_all_id_list?appkey=721651879&sign=4A68DE9911EF25401D0D09875EE4CEC3CA595D73&city=%E5%8C%97%E4%BA%AC',
+	// headers: {
+	// 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+	// }
 };
 
 var req = http.request(options, function(res) {
@@ -15,8 +15,7 @@ var req = http.request(options, function(res) {
 	res.setEncoding('utf8');
 	res.on('data', function(chunk) {
 		console.log('BODY: ' + chunk);
-		var $ = cheerio.load(chunk);
-		console.log($('a[class="shopname"]').attr('title'));
+		// var $ = cheerio.load(chunk);
 	});
 });
 
