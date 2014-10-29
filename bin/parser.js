d66e8b89acc342c26b03553e8fcdd22876eaf4fe
= <=>
2014-10-29 16:50:02 +0800

    add parser to get deals

--- bin/parser.js
+++ bin/parser.js
@@ -1,21 +1,73 @@
 var http = require('http');
-var cheerio = require('cheerio');
 
-var options = {
+var idListOptions = {
 	hostname: 'api.dianping.com',
 	path: '/v1/deal/get_all_id_list?appkey=721651879&sign=4A68DE9911EF25401D0D09875EE4CEC3CA595D73&city=%E5%8C%97%E4%BA%AC',
-	// headers: {
-	// 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
-	// }
 };
 
-var req = http.request(options, function(res) {
-	// console.log('STATUS: ' + res.statusCode);
-	// console.log('HEADERS: ' + JSON.stringify(res.headers));
-	res.setEncoding('utf8');
+function getBatchDealsById(deal_ids) {
+	var batchDealsOptions = {
+		hostname: 'api.dianping.com',
+		path: '/v1/deal/get_batch_deals_by_id?appkey=721651879&sign=82AEB57F7B4D0CD2C4CCB400F5A3E8E36F725025&deal_ids=' + deal_ids.toString(),
+	}
+
+	console.log(batchDealsOptions);
+
+	var req = http.request(batchDealsOptions, function(res) {
+		var bodyChunk = '';
+
+		res.on('data', function(chunk) {
+			// console.log('got %d bytes of data', chunk.length);
+			bodyChunk += chunk;
+		});
+
+		res.on('end', function() {
+			var payload = JSON.parse(bodyChunk);
+
+			if ('ERROR' === payload.status) {
+				console.log('errorCode: ', payload.error.errorCode);
+				console.log('errorMessage: ', payload.error.errorMessage);
+			} else {
+				console.log('count: ', payload.count);
+				console.log('deals.length: ', payload.deals.length);
+			}
+		});
+	});
+
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	req.end();
+}
+
+function getBatchDeals(start, end, array) {
+	if (end <= array.length) {
+		setTimeout(function() {
+			getBatchDealsById(array.slice(start, end));
+			getBatchDeals(start + 40, end + 40, array);
+		}, 1000);
+	}
+}
+
+var req = http.request(idListOptions, function(res) {
+	var bodyChunk = '';
+
 	res.on('data', function(chunk) {
-		console.log('BODY: ' + chunk);
-		// var $ = cheerio.load(chunk);
+		// console.log('got %d bytes of data', chunk.length);
+		bodyChunk += chunk;
+	});
+
+	res.on('end', function() {
+		var payload = JSON.parse(bodyChunk);
+
+		// console.log('Count: ', payload.count);
+		// console.log('id_list.length: ', payload.id_list.length);
+
+		var array = payload.id_list;
+		var size = payload.id_list.length;
+
+		getBatchDeals(0, 40 < size ? 40 : size, array);
 	});
 });
 
