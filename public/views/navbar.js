1fd5e29404891686c4c4364f104f1a1c6ffd0c85
= <=>
2015-01-09 17:38:41 +0800

    add login

--- public/views/navbar.js
+++ public/views/navbar.js
@@ -1,7 +1,23 @@
 define(['app', 'marionette', 'templates/compiled'], function(app, Marionette, JST) {
-
 	var Navbar = Marionette.ItemView.extend({
 		template: JST.NavbarTemplate,
+		events: {
+			'click button[type="submit"]': 'onSubmit'
+		},
+		onSubmit: function() {
+			var username = this.$el.find('#username').val();
+			var password = this.$el.find('#password').val();
+			$.ajax({
+				url: '/Login',
+				headers: {
+					Authorization: username + ':' + password
+				}
+			}).success(function(data, textStatus, jqXHR) {
+				console.log(textStatus);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				console.log(textStatus);
+			});
+		}
 	});
 
 	return Navbar;
