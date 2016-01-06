868efe44aff6bdf50acc0da3120f357b703d8c00
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-01-05 17:08:25 -0800

    add localforage success

--- public/views/db/localForage.js
+++ public/views/db/localForage.js
@@ -1,17 +1,20 @@
-define(['marionette', 'localforage', 'templates/compiled'], function(Marionette, LocalForage, JST) {
+define(['marionette', 'localforage', 'templates/compiled'], function(Marionette, localforage, JST) {
 	var LocalForage = Marionette.ItemView.extend({
 		template: JST.LocalForageTemplate,
 		className: 'local-forage',
 		initialize: function() {
-
+			this.setItem();
+			this.getItem();
 		},
 		setItem: function() {
-			LocalForage.setItem('key', 'localforage', function(err, result) {
-				console.log('setItem: ' + result.value);
+			localforage.setItem('key', 'localforage', function(err, result) {
+				if (err) console.log(err);
+				console.log('setItem: ' + result);
 			});
 		},
 		getItem: function() {
-			LocalForage.getItem('key', function(err, value) {
+			localforage.getItem('key', function(err, value) {
+				if (err) console.log(err);
 				console.log('getItem: ' + value);
 			});
 		}
