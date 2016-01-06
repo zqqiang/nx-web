df2854acdca950c186a97cc8a3d771945533618b
zqqiang <qiangzhaoqing@gmail.com>
2016-01-05 21:47:19 -0800

    add jquery animate

--- public/views/themes/creative.js
+++ public/views/themes/creative.js
@@ -1,7 +1,22 @@
 define(['marionette', 'templates/compiled'], function(Marionette, JST) {
 	var Creative = Marionette.ItemView.extend({
 		template: JST.CreativeTemplate,
-		className: 'creative'
+		className: 'creative',
+		ui: {
+			'more': 'a.page-scroll'
+		},
+		events: {
+			'click @ui.more': 'clickMore'
+		},
+		onRender: function() {
+
+		},
+		clickMore: function(event) {
+			this.$el.find('.creative').stop().animate({
+				scrollTop: (this.$el.find('a.page-scroll').offset().top - 50)
+			}, 1250, 'easeInOutExpo');
+			event.preventDefault();
+		}
 	});
 	return Creative;
 });
