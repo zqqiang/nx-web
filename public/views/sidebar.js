1ad36c4d4e8fe8c1deaf005aaffff0d0628dc749
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-01-12 17:29:06 -0800

    add jquery-slimscroll

--- public/views/sidebar.js
+++ public/views/sidebar.js
@@ -1,12 +1,16 @@
-// define(['marionette', 'templates/compiled'], function(Marionette, JST) {
 var Marionette = require('backbone.marionette');
 var SidebarHbs = require('../templates/sidebar.html');
+// require('jquery-slimscroll');
 
 var Sidebar = Marionette.ItemView.extend({
 	template: SidebarHbs,
 	tagName: 'section',
-	className: 'sidebar'
+	className: 'sidebar',
+	onShow: function() {
+		this.$el.find(".sidebar").slimScroll({
+			destroy: true
+		}).height("auto");
+	}
 });
 
 module.exports = Sidebar;
-// });
