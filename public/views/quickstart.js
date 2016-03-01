6aeab85d1221c3d92342de859557f1df73a2e2d2
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-02-29 17:31:12 -0800

    add react quickstart

--- public/views/quickstart.js
+++ public/views/quickstart.js
@@ -5,18 +5,49 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var QuickStart = Marionette.ItemView.extend({
-	template: QuickStartHbs,
-	className: 'quickstart',
-	onShow: function() {
-		var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
-		var window_height = $(window).height();
-		$(".content-wrapper, .right-side").css('min-height', window_height - neg);
+    template: QuickStartHbs,
+    className: 'quickstart',
+    onShow: function() {
+        var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
+        var window_height = $(window).height();
+        $(".content-wrapper, .right-side").css('min-height', window_height - neg);
 
-		ReactDOM.render(
- 		  <h1>Hello, world!</h1>,
- 		  document.getElementById('quick-start')
- 		);
-	}
+        var Title = React.createClass({
+            render: function() {
+                return (<h2>{this.props.title}</h2>);
+            }
+        });
+
+        var Content = React.createClass({
+            render: function() {
+                return (<h3>{this.props.content}</h3>);
+            }
+        });
+
+        var Paragraph = React.createClass({
+            render: function() {
+                var rows = [];
+                this.props.articles.forEach(function(article) {
+                    rows.push(<Title title={article.title} />);
+                    rows.push(<Content content={article.content} />);
+                });
+                return (<div>{rows}</div>);
+            }
+        });
+
+        var Articles = [{
+            title: 'nxManager',
+            content: 'This repository is a next generation central manager framework.'
+        }, {
+            title: 'Table of Contents',
+            content: '[Prepare](#prepare)'
+        }];
+
+        ReactDOM.render(
+            <Paragraph articles={Articles}/>,
+            document.getElementById('quick-start')
+        );
+    }
 });
 
 module.exports = QuickStart;
