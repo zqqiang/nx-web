4b635c36b7fdf04b88ae542e40773cfdf10883c0
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-03-10 16:06:33 -0800

    add sidebar toggle

--- public/views/header.js
+++ public/views/header.js
@@ -1,13 +1,15 @@
-// define(['marionette', 'templates/compiled'], function(Marionette, JST) {
+var $ = require('jquery');
 var Marionette = require('backbone.marionette');
 var HeaderHbs = require('../templates/header.html');
 
 var Header = Marionette.ItemView.extend({
-	template: HeaderHbs,
-	className: 'main-header',
-	initialize: function() {
-
-	},
+    template: HeaderHbs,
+    className: 'main-header',
+    events: {
+        'click a.sidebar-toggle': 'clickSidebar'
+    },
+    clickSidebar: function() {
+        $('body').toggleClass('sidebar-collapse');
+    }
 });
 module.exports = Header;
-// });
