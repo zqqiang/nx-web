4eead757a291cadb811b59f28e227aa34b6610d6
zqqiang <qiangzhaoqing@gmail.com>
2016-03-13 19:23:08 -0700

    add direct chat header

--- public/components/activity.react.js
+++ public/components/activity.react.js
@@ -18,19 +18,62 @@ var ContentHeader = React.createClass({
     }
 });
 
-var BoxHeader = React.createClass({
+// var BoxHeader = React.createClass({
+//     render: function() {
+//         return (
+//             <div className="box-header with-border">
+//                 <h3 className="box-title">Latest Members</h3>
+//                 <div className="box-tools pull-right">
+//                     <span className="label label-danger">8 New Members</span>
+//                     <button type="button" className="btn btn-box-tool" data-widget="collapse">
+//                         <i className="fa fa-minus"></i>
+//                     </button>
+//                     <button type="button" className="btn btn-box-tool" data-widget="remove">
+//                         <i className="fa fa-times"></i>
+//                     </button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// });
+
+var BoxHeaderSpan = React.createClass({
     render: function() {
         return (
+            <span data-toggle={this.props.span.toggle} title={this.props.span.title} className={this.props.span.className}>
+                {this.props.span.content}
+            </span>
+        );
+    }
+});
+
+var BoxHeaderButton = React.createClass({
+    render: function() {
+        return (
+            <button type="button" 
+                    className="btn btn-box-tool" 
+                    data-toggle={this.props.button.toggle} 
+                    title={this.props.button.title} 
+                    data-widget={this.props.button.widget}>
+                <i className={'fa fa-'+this.props.button.iclass}></i>
+            </button>
+        );
+    }
+});
+
+var BoxHeader = React.createClass({
+    render: function() {
+        var buttons = [];
+        this.props.buttons.forEach(function(button) {
+            buttons.push(<BoxHeaderButton button={button} key={button.iclass} />);
+        });
+
+        return (
             <div className="box-header with-border">
-                <h3 className="box-title">Latest Members</h3>
+                <h3 className="box-title">{this.props.title}</h3>
                 <div className="box-tools pull-right">
-                    <span className="label label-danger">8 New Members</span>
-                    <button type="button" className="btn btn-box-tool" data-widget="collapse">
-                        <i className="fa fa-minus"></i>
-                    </button>
-                    <button type="button" className="btn btn-box-tool" data-widget="remove">
-                        <i className="fa fa-times"></i>
-                    </button>
+                    <BoxHeaderSpan span={this.props.span}/>
+                    {buttons}
                 </div>
             </div>
         );
@@ -77,11 +120,26 @@ var LastestMembersItems = [
     { name: 'Nadia', date: '15 Jan', src: 'theme/project/img/member/user3-128x128.jpg' },
 ];
 
+var LatestMemebersButtons = [
+    { toggle: '', title: '', widget: 'collapse', iclass: 'minus' },
+    { toggle: '', title: '', widget: 'remove', iclass: 'times' },
+];
+
+var DirectChatButtons = [
+    { toggle: '', title: '', widget: 'collapse', iclass: 'minus' },
+    { toggle: 'tooltip', title: 'Contacts', widget: 'chat-pane-toggle', iclass: 'comments' },
+    { toggle: '', title: '', widget: 'remove', iclass: 'times' },
+];
+
+var LastestMembersSpan = { className: 'label label-danger', content: '8 New Members' };
+
+var DirectChatSpan = { toggle: 'tooltip', title: '3', className: 'badge bg-yellow', content: '3' }
+
 var LatestMembers = React.createClass({
     render: function() {
         return (
             <div className="box box-danger">
-                <BoxHeader />
+                <BoxHeader title='Latest Members' span={LastestMembersSpan} buttons={LatestMemebersButtons} />
                 <BoxBody items={LastestMembersItems} />
             </div>
         );
@@ -89,10 +147,10 @@ var LatestMembers = React.createClass({
 });
 
 var DirectChat = React.createClass({
-    render: function(){
+    render: function() {
         return (
             <div className="box box-warning direct-chat direct-chat-warning">
-
+                <BoxHeader title='Direct Chat' span={DirectChatSpan} buttons={DirectChatButtons} />
             </div>
         );
     }
