112b48c253555a10f6bb927f2232742277c6ccff
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-03-18 11:37:37 -0700

    udpate start port for openshift

--- app.js
+++ app.js
@@ -69,8 +69,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.set('port', process.env.PORT || 3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
+var server = app.listen(server_port, server_ip_address, function() {
+    console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });
