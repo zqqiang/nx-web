d4cd2b3a81e67381596591d7b61b6b2531a803ae
zqqiang <qiangzhaoqing@gmail.com>
2016-03-23 22:06:02 -0700

    add gallery

--- public/components/gallery.react.js
+++ public/components/gallery.react.js
@@ -73,11 +73,129 @@ var Pswp = React.createClass({
     }
 });
 
+var Carousel = React.createClass({
+    render: function(){
+        return (
+            <div className="row carousel-holder">
+                <div className="col-md-12">
+                    <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+                        <ol className="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+                        <div className="carousel-inner">
+                            <div className="item active">
+                                <img className="slide-image" src="http://placehold.it/800x300/39CCCC/ffffff" alt="" />
+                            </div>
+                            <div className="item">
+                                <img className="slide-image" src="http://placehold.it/800x300/39CCCC/ffffff" alt="" />
+                            </div>
+                            <div className="item">
+                                <img className="slide-image" src="http://placehold.it/800x300/39CCCC/ffffff" alt="" />
+                            </div>
+                        </div>
+                        <a className="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <i className="fa fa-chevron-left"></i>
+                        </a>
+                        <a className="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <i className="fa fa-chevron-right"></i>
+                        </a>
+                    </div>                    
+                </div>
+            </div>            
+        );
+    }
+});
+
+var StarItem = React.createClass({
+    render: function(){
+        return (
+            <i className={'fa fa-' + this.props.isgood === 'true' ? 'star' : 'star-o'}></i>
+        );
+    }
+});
+
+var Star = React.createClass({
+    render: function(){
+        var items = [];
+        for (var i = 0; i < this.props.stars; ++i) {
+            items.push(<StarItem isgood='true' key={i} />);
+        }
+        for (var i = 0; i < 5 - this.props.stars; ++i) {
+            items.push(<StarItem isgood='false' key={5-i} />);
+        }
+        return (
+            <p>
+                {items}
+            </p>
+        );
+    }
+});
+
+var ThumbnailItem = React.createClass({
+    render: function(){
+        return (
+            <div className="col-sm-4 col-lg-4 col-md-4">
+                <div className="thumbnail">
+                    <img src="http://placehold.it/320x150/39CCCC/ffffff" alt="" />
+                    <div className="caption">
+                        <h4 className="pull-right">{this.props.item.price}</h4>
+                        <h4>
+                            <a href="#">{this.props.item.name}</a>
+                        </h4>
+                        <p>{this.props.item.description}</p>
+                    </div>
+                    <div className="ratings">
+                        <p className="pull-right">{this.props.item.reviews + ' reviews'}</p>
+                        <Star stars={this.props.item.stars} />
+                    </div>
+                </div>                
+            </div>
+        );
+    }
+});
+
+var Thumbnail = React.createClass({
+    render: function() {
+        var items = [];
+        this.props.items.forEach(function(item) {
+            items.push(<ThumbnailItem item={item} key={item.name} />);
+        });
+
+        return (
+            <div className="row">
+                {items}
+            </div>
+        );
+    }
+});
+
+var GalleryItems = [
+    {price:'$24.99', name:'First Product', description:'xxxxxxxxxxxxxxx', reviews:'15', stars:'4'}
+];
+
+var GalleryView = React.createClass({
+    render: function(){
+        return (
+            <div className='container ecommerce'>
+                <div className="row">
+                    <div className="col-md-12">
+                        <Carousel />
+                        <Thumbnail items={GalleryItems} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
 var Gallery = React.createClass({
     render: function() {
         return (
             <div >
                 <Pswp />
+                <GalleryView />
             </div>
         );
     }
