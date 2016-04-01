db29ab9d7a888e246a1db49742f8134d8fb4880b
zqqiang <qiangzhaoqing@gmail.com>
2016-03-31 21:12:10 -0700

    add salon

--- public/components/salon.react.js
+++ public/components/salon.react.js
@@ -7,8 +7,7 @@ var UserBlock = React.createClass({
             <div className="user-block">
                 <img className="img-circle" src="http://placehold.it/100x100/39CCCC/ffffff" alt="User Image" />
                 <span className="username">
-                    <a href="#">
-                    </a>
+                    <a href="#">Jonathan Burke Jr.</a>
                 </span>
                 <span className="description">Shared publicly - 7:30 PM Today</span>
             </div>
@@ -51,9 +50,52 @@ var BoxBody = React.createClass({
             <div className="box-body">
                 <img className="img-responsive pad" src="http://placehold.it/2000x600/DD4B39/ffffff" alt="Photo" />
                 <p>I took this photo this morning. What do you guys think?</p>
-                <button type="button" className="btn btn-default btn-xs"><i className="fa fa-share"></i> Share</button>
+                <button type="button" className="btn btn-default btn-xs"><i className="fa fa-share"></i> Share</button>&nbsp;
                 <button type="button" className="btn btn-default btn-xs"><i className="fa fa-thumbs-o-up"></i> Like</button>
                 <span className="pull-right text-muted">127 likes - 3 comments</span>
+            </div>
+        );
+    }
+});
+
+var BoxComment = React.createClass({
+    render: function() {
+        return (
+            <div className="box-comment">
+                <img className="img-circle img-sm" src="http://placehold.it/50x50/DD4B39/ffffff" alt="User Image" />
+                <div className="comment-text">
+                    <span className="username">
+                        Maria Gonzales
+                        <span className="text-muted pull-right">8:03 PM Today</span>
+                    </span>
+                    It is a long established fact that a reader will be distracted
+                    by the readable content of a page when looking at its layout.
+                </div>
+            </div>
+        );
+    }
+});
+
+var BoxFooter = React.createClass({
+    render: function() {
+        return (
+            <div className="box-footer box-comments">
+                <BoxComment />
+            </div>
+        );
+    }
+});
+
+var BoxPostFooter = React.createClass({
+    render: function() {
+        return (
+            <div className="box-footer">
+                <form action="#" method="post">
+                    <img className="img-responsive img-circle img-sm" src="http://placehold.it/128x128/DD4B39/ffffff" alt="Alt Text" />
+                    <div className="img-push">
+                        <input type="text" className="form-control input-sm" placeholder="Press enter to post comment" />
+                    </div>
+                </form>
             </div>
         );
     }
@@ -65,6 +107,8 @@ var BoxWidget = React.createClass({
             <div className="box box-widget">
                 <BoxHeader />
                 <BoxBody />
+                <BoxFooter />
+                <BoxPostFooter />
             </div>
         );
     }
