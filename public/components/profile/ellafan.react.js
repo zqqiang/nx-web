8783fae8aa98c4410c0df9f2e523829f85d6951a
zqqiang <qiangzhaoqing@gmail.com>
2016-08-04 21:56:08 -0700

    add tab contents

--- public/components/profile/ellafan.react.js
+++ public/components/profile/ellafan.react.js
@@ -30,58 +30,101 @@ function ProfileImage() {
     )
 }
 
-function AboutMe() {
-    let cscUrl = 'https://www.linkedin.com/redir/redirect?url=https%3A%2F%2Fwww%2Ecsi%2Eca%2Fstudent%2Fverify%2FstudentBadge%2Edo%3Fcid%3DMzU4NDQ0NA%3D%3D&urlhash=ycDK&trk=profile_certification_company_title'
-    let sasAdvancedUrl = 'https://www.linkedin.com/redir/redirect?url=https%3A%2F%2Fwww%2Eyouracclaim%2Ecom%2Fbadges%2F1682ab90-dbb5-47a3-8a64-44a42ffa1d6a%2Flinked_in_profile&urlhash=eM3R&trk=profile_certification_company_title'
-    let sasBaseUrl = 'https://www.linkedin.com/redir/redirect?url=https%3A%2F%2Fwww%2Eyouracclaim%2Ecom%2Fbadges%2Fc4a64ce6-0983-4f3c-9978-c86aff60c4dd%2Flinked_in_profile&urlhash=wnc3&trk=profile_certification_company_title'
+function Experience({ icon, title, description }) {
+    return (
+        <div>
+            <strong><i className={"fa fa-" + icon + " margin-r-5"}></i> {title}</strong>
+            <p className="text-muted">{description}</p>
+            <hr />
+        </div>
+    )
+}
 
+let cscUrl = 'https://www.linkedin.com/redir/redirect?url=https%3A%2F%2Fwww%2Ecsi%2Eca%2Fstudent%2Fverify%2FstudentBadge%2Edo%3Fcid%3DMzU4NDQ0NA%3D%3D&urlhash=ycDK&trk=profile_certification_company_title'
+let sasAdvancedUrl = 'https://www.linkedin.com/redir/redirect?url=https%3A%2F%2Fwww%2Eyouracclaim%2Ecom%2Fbadges%2F1682ab90-dbb5-47a3-8a64-44a42ffa1d6a%2Flinked_in_profile&urlhash=eM3R&trk=profile_certification_company_title'
+let sasBaseUrl = 'https://www.linkedin.com/redir/redirect?url=https%3A%2F%2Fwww%2Eyouracclaim%2Ecom%2Fbadges%2Fc4a64ce6-0983-4f3c-9978-c86aff60c4dd%2Flinked_in_profile&urlhash=wnc3&trk=profile_certification_company_title'
+
+function Skills() {
+    return (
+        <div>
+            <strong><i className="fa fa-pencil margin-r-5"></i> Skills</strong>
+            <p>
+                <span className="label label-danger">CFA III Candidate</span>
+            </p>
+            <p>
+                <span className="label label-success">
+                    <a href={cscUrl}>Canadian Securities Course</a>
+                </span>
+            </p>
+            <p>
+                <span className="label label-info">
+                    <a href={sasAdvancedUrl}>SAS Certified Advanced Programmer for SAS 9</a>
+                </span>
+            </p>
+            <p>
+                <span className="label label-warning">
+                    <a href={sasBaseUrl}>SAS Certified Base Programmer for SAS 9</a>
+                </span>
+            </p>
+            <hr />
+        </div>
+    )
+}
+
+function AboutMe() {
     return (
         <div className="box box-primary">
             <div className="box-header with-border">
                 <h3 className="box-title">About Me</h3>
             </div>
             <div className="box-body">
-                <strong><i className="fa fa-book margin-r-5"></i> Education</strong>
-
-                <p className="text-muted">
-                    B.S. in Finance from the Nankai University at Tianjin China
-                </p>
-
-                <hr />
-
-                <strong><i className="fa fa-map-marker margin-r-5"></i> Location</strong>
-
-                <p className="text-muted">Burnaby, British Columbia</p>
-
-                <hr />
-
-                <strong><i className="fa fa-pencil margin-r-5"></i> Skills</strong>
-
-                <p>
-                    <span className="label label-danger">CFA III Candidate</span>
-                </p>
-                <p>
-                    <span className="label label-success">
-                        <a href={cscUrl}>Canadian Securities Course</a>
-                    </span>
-                </p>
-                <p>
-                    <span className="label label-info">
-                        <a href={sasAdvancedUrl}>SAS Certified Advanced Programmer for SAS 9</a>
-                    </span>
-                </p>
-                <p>
-                    <span className="label label-warning">
-                        <a href={sasBaseUrl}>SAS Certified Base Programmer for SAS 9</a>
-                    </span>
-                </p>
-
-                <hr />
-
-                <strong><i className="fa fa-file-text-o margin-r-5"></i> Notes</strong>
-
-                <p></p>
+                <Experience icon="book" title="Education" description="B.S. in Finance from the Nankai University at Tianjin China" />
+                <Experience icon="map-marker" title="Location" description="Burnaby, British Columbia" />
+                <Skills />
             </div>
+        </div>
+    )
+}
+
+function Tabs() {
+    return (
+        <ul className="nav nav-tabs">
+            <li className="active"><a href="#activity" data-toggle="tab">Activity</a></li>
+            <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
+            <li><a href="#settings" data-toggle="tab">Settings</a></li>
+        </ul>
+    )
+}
+
+function Post() {
+    return (
+        <div className="post">
+            
+        </div>
+    )
+}
+
+function TabPane() {
+    return (
+        <div className="active tab-pane" id="activity">
+            <Post />
+        </div>
+    )
+}
+
+function TabContents() {
+    return (
+        <div className="tab-content">
+            <TabPane />
+        </div>
+    )
+}
+
+function NavTabs() {
+    return (
+        <div className="nav-tabs-custom">
+            <Tabs />
+            <TabContents />
         </div>
     )
 }
@@ -96,6 +139,7 @@ export default function EllaFan() {
                         <AboutMe />
                     </div>
                     <div className="col-md-9">
+                        <NavTabs />
                     </div>
                 </div>
             </section>
