f5ab4fe9f7f81b25799f720d78880219f3b828a3
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-09-06 18:37:42 -0700

    remove icheck, refact login form

--- public/components/login.react.js
+++ public/components/login.react.js
@@ -1,54 +1,73 @@
 var React = require('react');
 
+function LoginForm() {
+	return (
+		<div className="">
+			<div className="form-group has-feedback">
+				<input type="email" className="form-control" placeholder="Email" />
+				<span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+			</div>
+			<div className="form-group has-feedback">
+				<input type="password" className="form-control" placeholder="Password" />
+				<span className="glyphicon glyphicon-lock form-control-feedback"></span>
+			</div>
+			<div className="row">
+				<div className="col-xs-8">
+					<div className="checkbox">
+						<label>
+							<input type="checkbox" /> Remember Me
+						</label>
+					</div>
+				</div>
+				<div className="col-xs-4">
+					<button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function LoginBoxBody() {
+	return (
+		<div className="login-box-body">
+			<p className="login-box-msg">Sign in to start your session</p>
+			<LoginForm />
+
+			<div className="social-auth-links text-center">
+				<p>- OR -</p>
+				<a href="javascript:void(0);" className="btn btn-block btn-social btn-facebook btn-flat">
+					<i className="fa fa-facebook"></i> Sign in using Facebook
+				</a>
+				<a href="javascript:void(0);" className="btn btn-block btn-social btn-google btn-flat">
+					<i className="fa fa-google-plus"></i> Sign in using Google+
+				</a>
+			</div>
+
+			<a href="javascript:void(0);">I forgot my password</a><br />
+			<a href="#Register" className="text-center">Register a new membership</a>
+
+		</div>
+	)
+}
+
+function LoginBody() {
+	return (
+		<div className="login-box">
+			<div className="login-logo">
+				<a href="javascript:void(0);"><b>nx</b>Manager</a>
+			</div>
+			<LoginBoxBody />
+		</div>
+	)
+}
+
 var Login = React.createClass({
     render: function() {
         return (
         	<div className="lockscreen">
-        	<div className="lockscreen-wrapper">
-			<div className="login-box">
-			  <div className="login-logo">
-			    <a href="../../index2.html"><b>Admin</b>LTE</a>
-			  </div>
-			  <div className="login-box-body">
-			    <p className="login-box-msg">Sign in to start your session</p>
-
-			    <form method="post">
-			      <div className="form-group has-feedback">
-			        <input type="email" className="form-control" placeholder="Email" />
-			        <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
-			      </div>
-			      <div className="form-group has-feedback">
-			        <input type="password" className="form-control" placeholder="Password" />
-			        <span className="glyphicon glyphicon-lock form-control-feedback"></span>
-			      </div>
-			      <div className="row">
-			        <div className="col-xs-8">
-			          <div className="checkbox icheck">
-			            <label>
-			              <input type="checkbox" /> Remember Me
-			            </label>
-			          </div>
-			        </div>
-			        <div className="col-xs-4">
-			          <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
-			        </div>
-			      </div>
-			    </form>
-
-			    <div className="social-auth-links text-center">
-			      <p>- OR -</p>
-			      <a href="#" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook"></i> Sign in using
-			        Facebook</a>
-			      <a href="#" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus"></i> Sign in using
-			        Google+</a>
-			    </div>
-
-			    <a href="#">I forgot my password</a><br />
-			    <a href="register.html" className="text-center">Register a new membership</a>
-
-			  </div>
-			</div>
-			</div>
+				<div className="lockscreen-wrapper">
+					<LoginBody />
+				</div>
 			</div>
         );
     }
