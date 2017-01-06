d12d4bdc824131ce210d635867df334a7593481a
zqqiang <qiangzhaoqing@gmail.com>
2017-01-06 07:54:47 -0800

    add google login callback

--- public/components/google.react.js
+++ public/components/google.react.js
@@ -1,26 +1,41 @@
 import React, { PropTypes } from 'react'
 
-const Google = ({onHandleClick}) => {
-	componentWillMount = () => {
+class Google extends React.Component {
+	componentWillMount() {
         const script = document.createElement("script");
 
-        script.src = "https://apis.google.com/js/platform.js?onload=renderButton";
+        script.src = "https://apis.google.com/js/platform.js";
         script.async = true;
 
         document.body.appendChild(script);
-	},
-    return (
-        <div className="login-page">
-	        <div className="login-box">
-				<div className="login-logo">
-            		<a href="javascript:void(0);"><b>nx</b>-manager</a>
-        		</div>
-        		<div className="login-box-body">
-	            	<div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-        		</div>
+	}
+	componentDidMount() {
+		gapi.signin2.render('g-signin2', {
+			'scope': 'profile email',
+			'width': 200,
+			'height': 50,
+			'longtitle': true,
+			'theme': 'dark',
+			'onsuccess': this.onSignIn
+		});
+	}
+	render() {
+	    return (
+	        <div className="login-page">
+		        <div className="login-box">
+					<div className="login-logo">
+	            		<a href="javascript:void(0);"><b>nx</b>-manager</a>
+	        		</div>
+	        		<div className="login-box-body">
+		            	<div className="g-signin2"></div>
+	        		</div>
+		        </div>
 	        </div>
-        </div>
-    )
+	    )
+	}
+	onSignIn(googleUser) {
+		console.log("googleUser Login");
+	}
 }
 
 export default Google
