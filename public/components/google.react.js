34362222be67055ad93298a7c2e2f594621d4806
zqqiang <qiangzhaoqing@gmail.com>
2017-01-10 22:47:50 -0800

    add google button style

--- public/components/google.react.js
+++ public/components/google.react.js
@@ -1,41 +1,48 @@
 import React, { PropTypes } from 'react'
+import GoogleLogin from 'react-google-login'
+
+const buttonStyle = {
+    display:'inline-block',
+    background:'#d14836',
+    color:'#fff',
+    width:'190px',
+    paddingTop:'10px',
+    paddingBottom:'10px',
+    borderRadius:'2px',
+    border:'1px solid transparent',
+    fontSize:'16px',
+    fontWeight:'bold',
+    fontFamily:'Roboto'
+}
 
 class Google extends React.Component {
-	componentWillMount() {
-        const script = document.createElement("script");
-
-        script.src = "https://apis.google.com/js/platform.js";
-        script.async = true;
-
-        document.body.appendChild(script);
-	}
-	componentDidMount() {
-		gapi.signin2.render('g-signin2', {
-			'scope': 'profile email',
-			'width': 200,
-			'height': 50,
-			'longtitle': true,
-			'theme': 'dark',
-			'onsuccess': this.onSignIn
-		});
-	}
-	render() {
-	    return (
-	        <div className="login-page">
-		        <div className="login-box">
-					<div className="login-logo">
-	            		<a href="javascript:void(0);"><b>nx</b>-manager</a>
-	        		</div>
-	        		<div className="login-box-body">
-		            	<div className="g-signin2"></div>
-	        		</div>
-		        </div>
-	        </div>
-	    )
-	}
-	onSignIn(googleUser) {
-		console.log("googleUser Login");
-	}
+    responseGoogle(response) {
+        console.log(response);
+    }
+    render() {
+        return (
+            <div className="login-page">
+                <div className="login-box">
+                    <div className="login-logo">
+                        <a href="javascript:void(0);"><b>nx</b>-manager</a>
+                    </div>
+                    <div className="login-box-body">
+                    <div id="customBtn" className="customGPlusSignIn">
+                        <GoogleLogin
+                            clientId={'658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'}
+                            onSuccess={this.responseGoogle}
+                            onFailure={this.responseGoogle}
+                            offline={false}
+                        >
+                            <span className="icon"></span>
+                            <span className="buttonText">Google</span>
+                        </GoogleLogin>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Google
