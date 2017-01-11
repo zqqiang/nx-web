a23002a09b9900e89df20461af9f6e184d45411c
zqqiang <qiangzhaoqing@gmail.com>
2017-01-11 10:13:04 -0800

    udpate google button

--- public/components/google.react.js
+++ public/components/google.react.js
@@ -2,17 +2,18 @@ import React, { PropTypes } from 'react'
 import GoogleLogin from 'react-google-login'
 
 const buttonStyle = {
-    display:'inline-block',
-    background:'#d14836',
-    color:'#fff',
-    width:'190px',
-    paddingTop:'10px',
-    paddingBottom:'10px',
-    borderRadius:'2px',
-    border:'1px solid transparent',
-    fontSize:'16px',
-    fontWeight:'bold',
-    fontFamily:'Roboto'
+  display: 'inline-block',
+  background: 'white',
+  color: '#444',
+  borderRadius: '5px',
+  border: 'thin solid #888',
+  boxShadow: '1px 1px 1px grey',
+  whiteSpace: 'nowrap',
+  flexGrow:'1',
+}
+
+const googleContainer = {
+    display: 'flex',
 }
 
 class Google extends React.Component {
@@ -26,18 +27,17 @@ class Google extends React.Component {
                     <div className="login-logo">
                         <a href="javascript:void(0);"><b>nx</b>-manager</a>
                     </div>
-                    <div className="login-box-body">
-                    <div id="customBtn" className="customGPlusSignIn">
+                    <div style={googleContainer}>
                         <GoogleLogin
                             clientId={'658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'}
                             onSuccess={this.responseGoogle}
                             onFailure={this.responseGoogle}
                             offline={false}
+                            style={buttonStyle}
                         >
                             <span className="icon"></span>
                             <span className="buttonText">Google</span>
                         </GoogleLogin>
-                    </div>
                     </div>
                 </div>
             </div>
