66b1f23333e8acb35b196ac65622a8bfb0f64c54
zqqiang <qiangzhaoqing@gmail.com>
2018-05-09 23:10:24 -0700

    add main index.html

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] }
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'public'),
+    port: 3000
+  },
+  plugins: [ new webpack.HotModuleReplacementPlugin() ]
+};
