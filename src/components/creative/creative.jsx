8246dcc3e7794370afc850673f23ca8670f366ce
zqqiang <qiangzhaoqing@gmail.com>
2018-05-10 22:16:54 -0700

    add nav

---
+++ src/components/creative/creative.jsx
@@ -0,0 +1,14 @@
+import React, { Component} from "react"
+import Nav from "./nav.jsx"
+
+class Creative extends Component{
+  render(){
+    return(
+      <div>
+        <Nav />
+      </div>
+    )
+  }
+}
+
+export default Creative
