1e47ea1fbb35ae2b521741e24543d33583187296
zqqiang <qiangzhaoqing@gmail.com>
2018-05-13 17:32:17 -0700

    update shop

--- shop/src/components/shop.jsx
+++ shop/src/components/shop.jsx
@@ -1,28 +1,15 @@
 import React, { Component} from "react"
 import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: white;
-`;
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: palevioletred;
-  height: 120px;
-  width: 950px;
-  padding: 0;
+const Header = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 class Shop extends Component{
   render(){
     return(
-      <Wrapper>
-          <Title>
-            奇奇海外科技
-          </Title>
-      </Wrapper>
+      <Header></Header>
     )
   }
 }
