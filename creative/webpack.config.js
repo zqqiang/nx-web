3816902bab2cf62a9b2e1aaf3cf74fc5324ba6d4
zqqiang <qiangzhaoqing@gmail.com>
2018-06-12 21:25:48 -0700

    update webpack

--- creative/webpack.config.js
+++ creative/webpack.config.js
@@ -1,49 +1,59 @@
 const path = require("path");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/main.js",
-  module: {
-    rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS
+    entry: "./src/main.js",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }, {
+                test: /\.scss$/,
+                use: [
+                    // fallback to style-loader in development
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+                ]
+            }, {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader', {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true
+                        }
+                    }
+                ]
+            }
         ]
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-            },
-          },
-        ],
-      }
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 3000
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output both options are
+            // optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    port: 3000
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
