dabab4a6b04c7a3c64179b135373705ae112d5a6
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-06-21 08:49:29 -0700

    add education

---
+++ react-bootstrap-resume/src/components/education.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from "react"
+
+import bcitLogo from "../img/bcit.png"
+
+class Education extends Component {
+    render() {
+        return (
+            <section
+                className="resume-section p-3 p-lg-5 d-flex flex-column"
+                id="education">
+                <div className="my-auto">
+                    <h2 className="mb-5">Education</h2>
+
+                    <div className="resume-item d-flex flex-column flex-md-row mb-5">
+                        <div className="resume-content mr-auto">
+                            <h3 className="mb-0">British Columbia Institute of Technology</h3>
+                            <div className="subheading mb-3">Certificate</div>
+                            <div>Applied Data Analytics</div>
+                            <p>GPA: 3.9</p>
+                        </div>
+                        <div className="resume-date text-md-right">
+                            <span className="text-primary">August 2016 - May 2019</span>
+                        </div>
+                    </div>
+
+                    <div className="resume-item d-flex flex-column flex-md-row">
+                        <div className="resume-content mr-auto">
+                            <h3 className="mb-0">Nanka University</h3>
+                            <div className="subheading mb-3">Bachelor's degree</div>
+                            <div>Finance</div>
+                            <p>GPA: 3.56</p>
+                        </div>
+                        <div className="resume-date text-md-right">
+                            <span className="text-primary">August 2004 - May 2008</span>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Education
--- react-bootstrap-resume/src/components/resume.jsx
+++ react-bootstrap-resume/src/components/resume.jsx
@@ -3,6 +3,7 @@ import React, {Component} from "react"
 import Nav from "./nav.jsx"
 import About from "./about.jsx"
 import Experience from "./experience";
+import Education from "./education";
 
 class Resume extends Component {
     render() {
@@ -12,6 +13,7 @@ class Resume extends Component {
                 <div className="container-fluid p-0">
                     <About/>
                     <Experience/>
+                    <Education/>
                 </div>
             </div>
         )
