8972a2dfb63313bddff4d47716b60bb14723ba98
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-07-18 08:18:01 -0700

    fix action import

--- react-create-app/src/stores/settingsStore.jsx
+++ react-create-app/src/stores/settingsStore.jsx
@@ -1,49 +1,60 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 
 export class SettingsStore {
   @observable httpPort = 80;
-  @observable httpsPort = 443
-  @observable telnetPort = 23
-  @observable sshPort = 22
-  @observable idleTimeout = 480
-  @observable redirectToHttps = true
-  @observable timezone = 4
-  @observable server = 'fortiguard'
-  @observable serverDomain = ''
-  @observable syncWithNtpServer = false
-  @observable syncInterval = 200
+  @observable httpsPort = 443;
+  @observable telnetPort = 23;
+  @observable sshPort = 22;
+  @observable idleTimeout = 480;
+  @observable redirectToHttps = true;
+  @observable timezone = 4;
+  @observable server = 'fortiguard';
+  @observable serverDomain = '';
+  @observable syncWithNtpServer = false;
+  @observable syncInterval = 200;
 
-  @action setHttpPort(port) {
+  @action
+  setHttpPort(port) {
     this.httpPort = port;
   }
-  @action setHttpsPort(port) {
+  @action
+  setHttpsPort(port) {
     this.httpsPort = port;
   }
-  @action setTelnetPort(port) {
+  @action
+  setTelnetPort(port) {
     this.telnetPort = port;
   }
-  @action setSshPort(port) {
+  @action
+  setSshPort(port) {
     this.sshPort = port;
   }
-  @action setIdleTimeout(timeout) {
+  @action
+  setIdleTimeout(timeout) {
     this.idleTimeout = timeout;
   }
-  @action setRedirectToHttps(redirect) {
+  @action
+  setRedirectToHttps(redirect) {
     this.redirectToHttps = redirect;
   }
-  @action setTimezone(timezone) {
+  @action
+  setTimezone(timezone) {
     this.timezone = timezone;
   }
-  @action setServer(server) {
+  @action
+  setServer(server) {
     this.server = server;
   }
-  @action setServerDomain(domain) {
+  @action
+  setServerDomain(domain) {
     this.serverDomain = domain;
   }
-  @action setSyncWithNtpServer(sync) {
+  @action
+  setSyncWithNtpServer(sync) {
     this.syncWithNtpServer = sync;
   }
-  @action setSyncInterval(interval) {
+  @action
+  setSyncInterval(interval) {
     this.syncInterval = interval;
   }
 }
