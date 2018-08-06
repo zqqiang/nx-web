765db31c34e5b1975dd2158b583f7d1105201a39
zqqiang <qiangzhaoqing@gmail.com>
2018-08-06 09:35:13 -0700

    header done

--- react-create-app/src/components/Header/Header.jsx
+++ react-create-app/src/components/Header/Header.jsx
@@ -1,33 +1,105 @@
 import React from 'react';
 // import PropTypes from "prop-types";
+import classNames from 'classnames';
 
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
-const styles = theme => ({});
+import Search from '@material-ui/icons/Search';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({
+  input: {
+    margin: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  },
+  iconSmall: {
+    fontSize: 20
+  },
+  flex: {
+    flexGrow: 1
+  }
+});
 
 class Header extends React.Component {
   state = {
-    lower: true
+    includingLowerLevel: true
+  };
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
   };
   render() {
-    // const { classes } = this.props;
+    const { classes } = this.props;
 
     return (
       <FormGroup row>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={this.state.lower}
-              onChange={this.handleChange}
-              value="lower"
+        <div className={classes.flex}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.includingLowerLevel}
+                onChange={this.handleChange('includingLowerLevel')}
+                value="includingLowerLevel"
+              />
+            }
+            label="Including lower level"
+          />
+          <Input
+            placeholder="Search FortiGate"
+            className={classes.input}
+            inputProps={{ 'aria-label': 'Description' }}
+          />
+          <Button
+            variant="contained"
+            size="small"
+            color="primary"
+            className={classes.button}
+          >
+            <Search
+              className={classNames(classes.leftIcon, classes.iconSmall)}
             />
-          }
-          label="Including lower level"
-        />
+            Search
+          </Button>
+          <Button variant="outlined" className={classes.button}>
+            Add Filter
+            <ExpandMore />
+          </Button>
+          <span>Sort by</span>
+          <Button variant="outlined" className={classes.button}>
+            SN
+            <ExpandMore />
+          </Button>
+        </div>
+        <Button color="primary" className={classes.button}>
+          Inventory
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          color="primary"
+          className={classes.button}
+        >
+          Add FortiGate
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          color="primary"
+          className={classes.button}
+        >
+          Group Management
+          <ExpandMore />
+        </Button>
       </FormGroup>
     );
   }
