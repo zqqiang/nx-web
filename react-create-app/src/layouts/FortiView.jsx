06e839dd61e0ff21c86d86943ceb66cd62e2bc5e
zqqiang <qiangzhaoqing@gmail.com>
2018-08-11 22:03:52 -0700

    add fortiview sidebar

--- react-create-app/src/layouts/FortiView.jsx
+++ react-create-app/src/layouts/FortiView.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
 
-const styles = theme => ({});
+import FortiViewSidebar from 'components/Sidebar/FortiViewSidebar';
+
+import routes from 'routes/view';
+
+const styles = theme => ({
+  wrapper: {
+    position: 'relative',
+    top: '0',
+    height: '100vh',
+    '&:after': {
+      display: 'table',
+      clear: 'both',
+      content: '" "'
+    }
+  }
+});
+
+const switchRoutes = (
+  <Switch>
+    {routes.map((prop, key) => {
+      if (prop.redirect)
+        return <Redirect from={prop.path} to={prop.pathTo} key={key} />;
+      if (prop.collapse)
+        return prop.views.map((prop, key) => {
+          return (
+            <Route path={prop.path} component={prop.component} key={key} />
+          );
+        });
+      return <Route path={prop.path} component={prop.component} key={key} />;
+    })}
+  </Switch>
+);
 
 class FortiView extends React.Component {
   render() {
-    const { classes } = this.props;
+    const { classes, ...rest } = this.props;
 
-    return <div className={classes.content}>FortiView</div>;
+    return (
+      <div className={classes.wrapper}>
+        <FortiViewSidebar routes={routes} {...rest} />
+        <div className={classes.mainPanel}>
+          <div className={classes.content}>
+            <div className={classes.containder}>{switchRoutes}</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ react-create-app/src/routes/view.jsx
@@ -0,0 +1,12 @@
+import SettingsIcon from '@material-ui/icons/Settings';
+
+const viewRoutes = [
+  {
+    path: '/fos/analysis/fortiview/summary',
+    name: 'Summary',
+    mini: 'S',
+    icon: SettingsIcon
+  }
+];
+
+export default viewRoutes;
